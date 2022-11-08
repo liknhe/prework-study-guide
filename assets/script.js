3025fd5778d0c52c49f1365e19b4d23f02087d42
liknhe <likhe@gmail.com>
2022-11-08 23:55:29 +0000

    Updated JS section and added additional JS functionalities

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,37 @@
 /*
-var topic = "JavaScript";
-
-
-if(topic === 'HTML') {
-    console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-    console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-    console.log("Let's study JavaScript!");
-} else {
-    console.log("Please try again!");
+function helloWorld() {
+    console.log("Hello, world, I am a function!");
 }
 
-
-var shapes = ["triangles", "square", "pentagon", "cirlce"];
-
-// console.log(shapes[0]);
-// console.log(shapes[1]);
-// console.log(shapes[2]);
-// console.log(shapes[3]);
-
-for (var x = 0; x < shapes.length; x++) {
-    console.log(shapes[x]);
-}
+helloWorld();
 */
 
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
-for (var y = 0; y < topics.length; y++) {
-    console.log(topics[y]);
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics(){
+    for (var y = 0; y < topics.length; y++) {
+        console.log(topics[y]);
+    }
 }
+
+function selectTopic(){
+    if(randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log("Please try again!");
+    }
+}
+
+console.log('Here are the topics we learned through Prework!');
+listTopics()
+
+console.log('Which topic should we study first?');
+selectTopic()
+
