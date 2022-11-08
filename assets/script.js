a69e216916601d24a041d5f22c885f5d315f5296
liknhe <likhe@gmail.com>
2022-11-08 23:37:36 +0000

    added JavaScript for loop to interate a topic array

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+/*
+var topic = "JavaScript";
+
+
+if(topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+    console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+} else {
+    console.log("Please try again!");
+}
+
+
+var shapes = ["triangles", "square", "pentagon", "cirlce"];
+
+// console.log(shapes[0]);
+// console.log(shapes[1]);
+// console.log(shapes[2]);
+// console.log(shapes[3]);
+
+for (var x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+}
+*/
+
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+for (var y = 0; y < topics.length; y++) {
+    console.log(topics[y]);
+}
